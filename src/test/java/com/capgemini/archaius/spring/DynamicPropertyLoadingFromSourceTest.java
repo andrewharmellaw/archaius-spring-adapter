50dc1dbf70f038b1177f957f46113136b88d7326
nwalter <nick.walter@capgemini.com>
2014-04-24 10:16:33 +0100

    Code review fixes

--- src/test/java/com/capgemini/archaius/spring/DynamicPropertyLoadingFromSourceTest.java
+++ src/test/java/com/capgemini/archaius/spring/DynamicPropertyLoadingFromSourceTest.java
@@ -38,7 +38,7 @@ public class DynamicPropertyLoadingFromSourceTest {
     }
 
     @Test
-    public void changingThePropertiesFileUpdatesTheProperties() throws IOException, InterruptedException {
+    public void testChangingThePropertiesFileUpdatesTheProperties() throws IOException, InterruptedException {
 
         // check the value now
         propertyValue = DynamicPropertyFactory.getInstance().getStringProperty("var2", null).getValue();
@@ -62,7 +62,7 @@ public class DynamicPropertyLoadingFromSourceTest {
     }
 
     @Test
-    public void deletingPropertyValueInFileRemovesTheProperties() throws IOException, InterruptedException {
+    public void testDeletingPropertyValueInFileRemovesTheProperties() throws IOException, InterruptedException {
         // check the value now
         propertyValue = DynamicPropertyFactory.getInstance().getStringProperty("var2", null).getValue();
         assertEquals("MY SECOND VAR", propertyValue);
