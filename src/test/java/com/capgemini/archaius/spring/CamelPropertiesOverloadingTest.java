2dff0fe616e256f69defa734c2e97f94bfc637a5
rhart <russellxhart@googlemail.com>
2014-05-27 22:44:41 +0100

    Closes #25

--- src/test/java/com/capgemini/archaius/spring/CamelPropertiesOverloadingTest.java
+++ src/test/java/com/capgemini/archaius/spring/CamelPropertiesOverloadingTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 Capgemini (oss@capgemini.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.capgemini.archaius.spring;
 
 import org.apache.camel.CamelContext;
@@ -6,14 +21,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
-import org.springframework.beans.factory.annotation.Value;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  *
@@ -25,12 +40,11 @@ import org.springframework.beans.factory.annotation.Value;
 public class CamelPropertiesOverloadingTest {
     
     private final String camelPropertyKey = "{{var2}}";
-    private String camelPropertyValue;
-    @Value("${var2}") private String springPropertyValue;
-    
     @Autowired
     @Qualifier("camel")
     protected CamelContext context;
+    private String camelPropertyValue;
+    @Value("${var2}") private String springPropertyValue;
     
     @DirtiesContext
     @Test
